099de39c75262d4c62fc26ff481e045ea17df2d9
tim.rohrer <tim@tgr66.dev>
2022-04-21 16:55:49 -0500

    style: Change order of imports

--- src/common/custom_errors/AuthorizationError.ts
+++ src/common/custom_errors/AuthorizationError.ts
@@ -1,5 +1,5 @@
+import { HttpStatusCode } from "../http-status-codes.js"
 import BaseError from "./BaseError.js"
-import { HttpStatusCode } from "./http-status-codes.js"
 
 export default class AuthorizationError extends BaseError {
   httpCode = HttpStatusCode.FORBIDDEN
