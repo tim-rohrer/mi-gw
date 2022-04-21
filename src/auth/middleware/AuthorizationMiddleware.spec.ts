470398a9ecdd94c951682fc3da993fe18885922f
tim.rohrer <tim@tgr66.dev>
2022-04-21 17:02:35 -0500

    test: Refactor test to reflect new custom error

--- src/auth/middleware/AuthorizationMiddleware.spec.ts
+++ src/auth/middleware/AuthorizationMiddleware.spec.ts
@@ -1,8 +1,7 @@
 import { getMockReq, getMockRes } from "@jest-mock/express"
 
-import { ErrorTypeTitle } from "../../common/ErrorTypeTitle.js"
+import AuthorizationError from "../../common/custom_errors/AuthorizationError.js"
 import { HttpStatusCode } from "../../common/http-status-codes.js"
-import HttpError from "../../common/HttpError.js"
 import { AuthorizationMiddleware } from "./AuthorizationMiddleware.js"
 
 const { res, next, mockClear } = getMockRes()
@@ -11,9 +10,7 @@ describe("Authorization Middleware", () => {
   beforeEach(() => mockClear())
 
   it("should verify the presented API Token from the authorization middleware object", () => {
-    const errorResponse = new HttpError(
-      ErrorTypeTitle.NOT_AUTHORIZED,
-      HttpStatusCode.UNAUTHORIZED,
+    const errorResponse = new AuthorizationError(
       "The presented API token does not permit you access to the requested service. Please request the additional authorization.",
     )
     const req = getMockReq({
@@ -36,9 +33,7 @@ describe("Authorization Middleware", () => {
           "NCUYlC7G0I77FaZTm0skchNswhAJIdfC0WrUNMcnlsG5G2NDe2VYcyr1EcH52bKV",
       },
     })
-    const expectedResponse = new HttpError(
-      ErrorTypeTitle.NOT_AUTHORIZED,
-      HttpStatusCode.UNAUTHORIZED,
+    const expectedResponse = new AuthorizationError(
       "The presented API token does not permit you access to the requested service. Please request the additional authorization.",
     )
     AuthorizationMiddleware.getInstance()
@@ -47,32 +42,4 @@ describe("Authorization Middleware", () => {
 
     expect(next).toHaveBeenCalledWith(expectedResponse)
   })
-  // it("should verify user's authorization from the database, and add to local.", async () => {
-  //   const geocodeRequest = googleGeocodeRequest
-  //   mockRequest = {
-  //     query: geocodeRequest,
-  //     body: geocodeRequest,
-  //   }
-  //   AuthorizationMiddleware.getInstance()
-  //   AuthorizationMiddleware.addLocalAuthorization(
-  //     "localTokenOnly",
-  //     "localUserOnly",
-  //   )
-  //   const spy = jest.spyOn(usersService, "isUserAuthorized").mockResolvedValue(<
-  //     OkImpl<string>
-  //   >{
-  //     ok: true,
-  //     err: false,
-  //     val: "goodDbUserId",
-  //   })
-
-  //   await AuthorizationMiddleware.isUserAuthorized(
-  //     <Request>mockRequest,
-  //     <Response>mockResponse,
-  //     nextFunction,
-  //   )
-
-  //   expect(spy).toHaveBeenCalledTimes(1)
-  //   expect(AuthorizationMiddleware.getLocalAuthorizations()).toHaveLength(2)
-  // })
 })
